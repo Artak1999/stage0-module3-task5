8d0ff2888126218512c59df5f57f9d2c7a9ec259
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 19:19:30 +0500

    It's a fun.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int a = toBeDivided / 19;
-        System.out.println(a);
+        System.out.println(toBeDivided / 5);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        float c = minutes / 60;
+        float c = minutes * 60;
         System.out.println(c);
     }
 }
